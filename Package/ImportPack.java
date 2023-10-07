f413e6779b7b5c4a66f30a373b15a27437d2639c
Gokul Lade <97846356+GokulLade@users.noreply.github.com>
2023-10-07 14:24:53 +0530

    Update ImportPack.java

--- Package/ImportPack.java
+++ Package/ImportPack.java
@@ -1,11 +1,12 @@
                 /* Import user define package Demo program */
 import Gokul.MyPack;
 
-public class ImportPack {
+public class ImportPack
+{
     public static void main(String[] args) 
     {
-        MyPack m=new MyPack();
-        m.add();
+        MyPack s=new MyPack();
+        s.add();
 
     }
     
